19f2c6450214757bf54494f5d7b66220db157106
danishshariff <danishshariff92@gmail.com>
2024-11-10 20:21:09 +0530

    unix 3 b

---
+++ unix3b.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+ #include<unistd.h>
+ int main(){
+ pid_tchild_pid;
+
+ child_pid = fork();
+ if (child_pid ==-1) {
+ std::cerr << "Fork failed." << std::endl;
+ return 1;
+ }
+ for (int i = 1; i <= 5; i++) {
+ if (child_pid == 0) {
+ 
+ std::cout << "Child Count: " << i << std::endl;
+ } else {
+ s
+ std::cout << "Parent Count: " << i << std::endl;
+ }
+ sleep(1); 
+ }
+ return 0;
+ }
